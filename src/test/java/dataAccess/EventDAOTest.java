7c809a6d780e72eeef348bc76badd3d3b10d1542
reganhanson <reganhanson1994>
2021-03-11 13:32:12 -0700

    Design Assignment finished

--- src/test/java/dataAccess/EventDAOTest.java
+++ src/test/java/dataAccess/EventDAOTest.java
@@ -1,5 +1,6 @@
 package dataAccess;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,10 +9,10 @@ import model.Event;
 import java.sql.Connection;
 
 class EventDAOTest {
+    @BeforeAll
 
     @Test
     void insertTest() {
-        // EventDao eventDao = new EventDAO();
     }
 
     @Test
